cb739c92e5344234bb9bdf2dd6f80828c7e5be3a
fivem <952241250@qq.com>
2023-08-26 23:04:23 +0800

    初始化数据管理页面

--- src/composables/exportDictValue.js
+++ src/composables/exportDictValue.js
@@ -1,9 +1,9 @@
 import { computed } from "vue";
 
-export async function exportDictValue(dict) {
+export async function exportDictValue(dict, key) {
   // 创建一个计算属性，将字典列表的 value 取出形成列表
   const valueList = computed(() => {
-    return dict.map((item) => item.Township);
+    return dict.map((item) => item[key]);
   });
   const dictList = valueList.value;
   return {
--- src/views/Manager/index.vue
+++ src/views/Manager/index.vue
@@ -4,49 +4,70 @@ import { onMounted, ref } from "vue";
 import { exportDictValue } from "@/composables/exportDictValue";
 
 const radio1 = ref("行政区划");
-const log = () => {
-  console.log(radio1);
+const activeList = ref([]);
+const optionMap = {
+  行政区划: "getTownship",
+  断面名称: "getSection",
+  河流名称: "getRiver",
+};
+const changeActiveList = () => {
+  const selectedOption = optionMap[radio1.value];
+  if (selectedOption) {
+    const getFunction = eval(selectedOption);
+    getFunction().then((list) => {
+      activeList.value = list;
+    });
+  }
 };
 
 import { getTownshipList } from "@/stores/getTownshipList";
 const getTownship = async () => {
   const res = await getTownshipList();
-  return exportDictValue(res.townshipList).then((result) => {
+  return exportDictValue(res.townshipList, "Township").then((result) => {
     const townshipList = result.dictList;
     return townshipList;
   });
 };
 
-const list = ref([]);
-getTownship().then((townshipList) => {
-  list.value = townshipList;
-  console.log(list.value);
-});
-
 import { getSectionList } from "@/stores/getSectionList";
-const getSection = async (townshipName) => {
-  const res = await getSectionList(townshipName);
-  const sectionList = res.sectionList;
+const getSection = async () => {
+  const res = await getSectionList();
+  return exportDictValue(res.sectionList, "SectionName").then((result) => {
+    const sectionList = result.dictList;
+    return sectionList;
+  });
 };
 
 import { getRiverList } from "@/stores/getRiverList";
-const getRiver = async (townshipName, activeSectionName) => {
-  const res = await getRiverList(townshipName, activeSectionName);
-  const riverList = res.riverList;
+const getRiver = async () => {
+  const res = await getRiverList();
+  return exportDictValue(res.riverList, "RiverName").then((result) => {
+    const riverList = result.dictList;
+    return riverList;
+  });
 };
 
 onMounted(() => {
-  getTownship(), getRiver(), getSection();
+  changeActiveList(radio1.value);
 });
+
+const activeName = ref("");
+const changeName = (item) => {
+  activeName.value = item;
+  console.log(activeName);
+};
 </script>
 <template>
   <LayoutHeader />
 
   <div class="common-layout">
     <el-container>
-      <el-aside width="400px"
-        >Aside
-        <el-radio-group v-model="radio1" size="large" @click="log()">
+      <el-aside>
+        <el-radio-group
+          text-color="#fff"
+          v-model="radio1"
+          @change="changeActiveList(radio1)"
+        >
           <el-radio-button label="行政区划">
             <strong>行政区划</strong>
           </el-radio-button>
@@ -57,17 +78,121 @@ onMounted(() => {
             <strong>河流名称</strong>
           </el-radio-button>
         </el-radio-group>
-        <div v-for="item in list" :key="item.Township">
-          {{ item.Township }}
+        <div class="list">
+          <div
+            :class="{ active: item == activeName }"
+            v-for="item in activeList"
+            :key="item"
+            @click="changeName(item)"
+          >
+            {{ item }}
+          </div>
         </div>
       </el-aside>
-      <el-main>Main</el-main>
+      <el-main>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main
+            ><el-table
+              id="table"
+              :data="qualityList"
+              stripe
+              style="width: 95%"
+              height="160"
+            >
+              <el-table-column prop="Date" label="时间">
+                <!-- <template #default="{ row }">
+            {{ row.Year }}/0{{ row.Quarter }}
+          </template> -->
+              </el-table-column>
+              <el-table-column prop="AmmoniaNitrogen" label="氨氮(mg/l)" />
+              <el-table-column prop="TotalPhosphorus" label="总磷(mg/l)" />
+              <el-table-column
+                prop="PermanganateIndex"
+                label="高锰酸盐(mg/l)"
+                width="162"
+              />
+              <el-table-column prop="DissolvedOxygen" label="溶解氧(mg/l)" />
+              <el-table-column prop="ComplianceStatus" label="达标情况" />
+            </el-table>
+          </el-main>
+        </el-container>
+      </el-main>
     </el-container>
   </div>
 </template>
 
 <style scoped lang="scss">
+.active {
+  color: #fff;
+  background-color: #0b96f0; // 设置选中状态下的背景色
+  border: 0px solid #000;
+}
 .common-layout {
   height: 789px;
+
+  .el-aside {
+    height: 720px;
+    width: 320px;
+    border-radius: 35px;
+    border: 1px solid #bbbbbb;
+
+    margin: 37px 18px 0;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .el-radio-group {
+      margin: 20px auto;
+
+      ::v-deep(.el-radio-button__inner) {
+        color: #409eff;
+      }
+    }
+    .list {
+      width: 200px;
+      font: 14px bold;
+      text-align: center;
+
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+
+      div {
+        height: 22px;
+        width: 90px;
+        border: 1px solid #bbb;
+        border-radius: 10px;
+
+        margin-right: 20px;
+        margin-bottom: 10px;
+
+        &:nth-child(2n) {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+
+  .el-main {
+    height: 720px;
+    border-radius: 35px;
+
+    margin: 37px 18px 0 0;
+    padding: 0;
+    .el-main {
+      height: 658px;
+      width: 100%;
+      border-radius: 35px;
+      border: 1px solid #bbbbbb;
+
+      margin: 0;
+
+      .el-table {
+        margin: 20px auto;
+      }
+    }
+  }
 }
 </style>
